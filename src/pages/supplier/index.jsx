82ec0249646d413328824505cd13b7e7e9550884
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2023-03-18 23:07:17 +0700

    fixed URLs

--- src/pages/supplier/index.jsx
+++ src/pages/supplier/index.jsx
@@ -65,7 +65,7 @@ export default function Home({ suppliers }) {
 }
 export async function getServerSideProps() {
     console.log("supplier/index.jsx". process.env.NEXT_PUBLIC_API_URL)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/supplier/`)
+    const res = await fetch(`https://stock-final-6228104.vercel.app/api/supplier/`)
     const suppliers = await res.json()
     console.log(suppliers)
     return { props: { suppliers } }
