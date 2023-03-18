98432cab7cb657249e67dc810afc1849350fbeba
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2023-03-19 01:27:42 +0700

    added back

--- src/pages/supplier/index.jsx
+++ src/pages/supplier/index.jsx
@@ -64,7 +64,7 @@ export default function Home({ suppliers }) {
     )
 }
 export async function getServerSideProps() {
-    const res = await fetch(`https://stock-final-6228104.vercel.app/api/supplier/`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/supplier/`)
     console.log(res)
     const suppliers = await res.json()
 
